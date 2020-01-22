6ff9fb0bf9575ed924e5a7dba97e2b7a0195e201
Burak Kaygusuz <kaygusuzburak@gmail.com>
2020-01-22 16:08:49 +0300

    Nested class name fixed

--- src/test/kotlin/googleapis/GetRequestTests.kt
+++ src/test/kotlin/googleapis/GetRequestTests.kt
@@ -18,7 +18,7 @@ class GetRequestTests {
     }
 
     @Nested
-    inner class `verify code should` {
+    inner class `the status code should` {
 
         val apiKey = "AIzaSyC-oGQs1Sx9YPWepK1-xC44qhQSzzU3iEk"
 
