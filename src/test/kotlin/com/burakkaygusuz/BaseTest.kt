cd45fedb52641e16b1b5299b79fcc27463b91ee6
Burak Kaygusuz <kaygusuzburak@gmail.com>
2021-02-19 09:54:11 +0300

    Set the required params as global through all tests

--- src/test/kotlin/com/burakkaygusuz/BaseTest.kt
+++ src/test/kotlin/com/burakkaygusuz/BaseTest.kt
@@ -1,6 +1,12 @@
 package com.burakkaygusuz
 
 import io.restassured.RestAssured
+import io.restassured.builder.RequestSpecBuilder
+import io.restassured.config.LogConfig
+import io.restassured.config.RestAssuredConfig
+import io.restassured.filter.log.LogDetail
+import io.restassured.http.ContentType
+import io.restassured.specification.RequestSpecification
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.TestInstance
@@ -8,11 +14,25 @@ import org.junit.jupiter.api.TestInstance
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 open class BaseTest {
 
+    companion object {
+        lateinit var requestSpecification: RequestSpecification
+        const val apiKey: String = "<insert-your-google-api-key>"
+    }
+
     @BeforeAll
     fun setUp() {
-        RestAssured.baseURI = "https://www.googleapis.com"
-        RestAssured.basePath = "/blogger/v3"
-        RestAssured.useRelaxedHTTPSValidation()
+
+        val logConfig = LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails(LogDetail.ALL)
+        val config = RestAssuredConfig.config().logConfig(logConfig)
+
+        requestSpecification = RequestSpecBuilder()
+            .setBaseUri("https://www.googleapis.com")
+            .setBasePath("/blogger/v3")
+            .addQueryParam("key", apiKey)
+            .setContentType(ContentType.JSON)
+            .setRelaxedHTTPSValidation()
+            .setConfig(config)
+            .build()
     }
 
     @AfterAll
--- src/test/kotlin/com/burakkaygusuz/GetRequestTests.kt
+++ src/test/kotlin/com/burakkaygusuz/GetRequestTests.kt
@@ -11,18 +11,16 @@ import org.hamcrest.CoreMatchers.not
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
-class GetRequestTests : BaseTest() {
-
-    private val apiKey: String = "<insert-your-google-api-key>"
+class `Get Request Tests` : BaseTest() {
 
     @Nested
-    inner class `the status code should` {
+    inner class `The status code should` {
 
         @Test
         fun `be 200 when the blogId is valid`() {
 
             Given {
-                queryParam("key", apiKey)
+                spec(requestSpecification)
             } When {
                 get("/blogs/3213900")
             } Then {
@@ -34,7 +32,7 @@ class GetRequestTests : BaseTest() {
         fun `be 404 when the blogId is invalid`() {
 
             Given {
-                queryParam("key", apiKey)
+                spec(requestSpecification)
             } When {
                 get("/blogs/6258425")
             } Then {
@@ -44,13 +42,13 @@ class GetRequestTests : BaseTest() {
     }
 
     @Nested
-    inner class `blogId should` {
+    inner class `BlogId should` {
 
         @Test
         fun `be equal to 3213900 when queries the related blog url`() {
 
             Given {
-                queryParam("key", apiKey)
+                spec(requestSpecification)
                 queryParam("url", "http://blogger-developers.googleblog.com")
             } When {
                 get("/blogs/byurl")
@@ -64,7 +62,7 @@ class GetRequestTests : BaseTest() {
         fun `not be equal to 3213900 when queries the unrelated blog url`() {
 
             Given {
-                queryParam("key", apiKey)
+                spec(requestSpecification)
                 queryParam("url", "https://google.com")
             } When {
                 get("/blogs/byurl")
